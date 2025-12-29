e7db3ef981466993e27edc9b78f4150584ce5e3b
Antonio <haritianaantoniorajohnson@gmail.com>
2025-12-29 18:40:13 +0300

    Create a class called LessonExtraction Two functions need to be combined: load_lessons and get_lessons, they use the same parameters filepath
    Also create a function called get_lessons since:
    custom_lessons = load_lessons()
    my_lesson = custom_lessons["lessons"]
    Are repeated to many times in the code

--- src/storage.py
+++ src/storage.py
@@ -2,16 +2,43 @@ import json
 import os
 
 
-def load_lessons(filepath):
-    """Load lessons from a JSON file."""
-    if not os.path.exists(filepath):
-        return []  # If file missing, return empty list, no crash
+# def load_lessons(filepath):
+#     """Load lessons from a JSON file."""
+#     if not os.path.exists(filepath):
+#         return []  # If file missing, return empty list, no crash
 
-    with open(filepath, "r", encoding="utf-8") as f:
-        """ "Convert from Json to python"""
-        global data
-        data = json.load(f)
-        return data
+#     with open(filepath, "r", encoding="utf-8") as f:
+#         """ "Convert from Json to python"""
+#         global data
+#         data = json.load(f)
+#         return data
+
+
+# def get_lessons():
+#     custom_lessons = load_lessons()
+#     my_lesson = custom_lessons["lessons"]
+#     return my_lesson
+
+
+class LessonExtraction:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def load_lessons(self):
+        """Load lessons from a JSON file."""
+        if not os.path.exists(self.filepath):
+            return []  # If file missing, return empty list, no crash
+
+        with open(self.filepath, "r", encoding="utf-8") as f:
+            """ "Convert from Json to python"""
+            global data
+            data = json.load(f)
+            return data
+
+    def get_lessons(self):
+        custom_lessons = self.load_lessons()
+        my_lesson = custom_lessons["lessons"]
+        return my_lesson
 
 
 def modify_lessons(lessons):
